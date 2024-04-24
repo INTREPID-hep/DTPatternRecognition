55262721756537133b42b8e7a22a2a5be23043e0
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2024-04-24 16:56:29 +0200

    Upload dumper

---
+++ dump_to_json.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import ROOT as r
+from root_numpy import tree2array
+from warnings import filterwarnings
+import json
+import sys
+import os
+
+filterwarnings(action='ignore', category=DeprecationWarning, message='`np.object` is a deprecated alias')
+
+path = "/eos/cms/store/user/folguera/INTREPID/DTShowers/2024_04_23/muon"
+
+list_files = [file for file in os.listdir(path) if "root" in file]
+
+# Make a tchain:
+ttree = r.TChain()
+
+for ifile in list_files:
+    ttree.Add( path + "/" + ifile + "/DTSim")
+
+#ttree = tfile.Get("DTSim")
+arr = tree2array(ttree)
+
+# -- Convertimos a dataframe
+df = pd.DataFrame(arr)
+
+# Assuming your DataFrame is named df
+
+# Filter and create JSON for each SLHit_SL value
+for sl in range(1, 4):  # SLHit_SL values range from 1 to 4
+    print(" Getting sl", sl)
+    sl_filtered = df[df['SLHit_SL'] == sl]
+    
+    out = []
+
+    for index, row in sl_filtered.iterrows():
+        out.append({
+            "event_eventNumber": row['EventNo'],
+            "digi_wheel": 0,
+            "digi_sector": 12,
+            "digi_station": 1,
+            "digi_superLayer": row['SLHit_SL'],
+            "digi_layer": row['SLHit_Layer'],
+            "digi_wire": row['SLHit_Cell'],
+            "digi_time": row['SLHit_Time'],
+            "digi_PDG": row['SLHit_PDG'],
+        }
+       )
+
+    filename = "jsonGeant4_sl{sl}.json".format(sl = sl)
+    with open(filename, 'w') as f:
+        json.dump(out, f, indent=4)
+    output_json = json.dumps(out, indent = 2)
+    print("JSON for SLHit_SL {sl}:".format(sl = sl))
+    print(output_json)
+    print("\n")
+
